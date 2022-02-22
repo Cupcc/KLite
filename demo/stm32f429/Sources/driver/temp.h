07dc7dd8785499795d55161423e9c95125776a3c
jiangxiaogang <kerndev@foxmail.com>
2022-02-22 23:10:45 +0800

    删除demo文件夹

--- demo/stm32f429/Sources/driver/temp.h
+++
@@ -1,7 +0,0 @@
-#ifndef __TEMP_H
-#define __TEMP_H
-
-void temp_init(void);
-int  temp_read(void);
-
-#endif
--- demo/stm32f429/Sources/main/log.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LOG_H
-#define __LOG_H
-
-void log_init(void);
-void log_printf(const char *fmt, ...);
-
-#define LOG   log_printf
-
-#endif
