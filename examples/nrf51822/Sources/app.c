c40d23621a49664cb924c2904e09615674217592
jiangxiaogang <kerndev@foxmail.com>
2022-02-27 22:45:42 +0800

    添加nrf51822参考代码

---
+++ examples/nrf51822/Sources/app.c
@@ -0,0 +1,30 @@
+#include "kernel.h"
+#include "bsp.h"
+
+static void demo_thread1(void *arg)
+{
+	while(1)
+	{
+		bsp_led_on(0);
+		thread_sleep(500);
+		bsp_led_off(0);
+		thread_sleep(500);
+	}
+}
+
+static void demo_thread2(void *arg)
+{
+	while(1)
+	{
+		//bsp_led_on(1);
+		thread_sleep(200);
+		//bsp_led_off(1);
+		thread_sleep(200);
+	}
+}
+
+void app_init(void)
+{
+	thread_create(demo_thread1, NULL, 256);
+	thread_create(demo_thread2, NULL, 256);
+}
