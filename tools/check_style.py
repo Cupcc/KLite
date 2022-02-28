f6f8f300ddb74c6967af18a65146ad0a286e54c4
jiangxiaogang <kerndev@foxmail.com>
2022-02-28 19:00:48 +0800

    超过10行错误跳过此文件

--- tools/check_style.py
+++ tools/check_style.py
@@ -10,15 +10,15 @@ def check_line_start(line):
 			return False
 		if(ch != "\t"):
 			return True
-	
+
 def check_line_end(line):
 	if line.endswith(" \r\n"):
 		return False
 	if line.endswith("\t\r\n"):
 		return False
 	return True
-	
-def check_line_crlf(line):
+
+def check_line_crlf(line, crlf):
 	cr = 0
 	lf = 0
 	for ch in line:
@@ -26,8 +26,15 @@ def check_line_crlf(line):
 			cr = cr + 1
 		if(ch == "\n"):
 			lf = lf + 1
-	if cr == 1 and lf == 1:
-		return True
+	if crlf == "windows":
+		if cr == 1 and lf == 1:
+			return True
+	if crlf == "unix":
+		if cr == 0 and lf == 1:
+			return True
+	if crlf == "mac":
+		if cr == 1 and lf == 0:
+			return True
 	return False
 	
 def check_space_line(line):
@@ -42,41 +49,72 @@ def check_space_line(line):
 			return False
 		return True
 	return False
-	
+
+def get_crlf_style(line):
+	if line.endswith("\r\n"):
+		return "windows"
+	elif line.endswith("\n"):
+		return "unix"
+	elif line.endswith("\r"):
+		return "mac"
+	return "error"
+
 def check_style(path):
 	#print(path)
-	file = io.open(path, "rb")
+	crlf = None
+	err_count = 0
 	line_num = 0
 	space_line = 0
+	file = io.open(path, "rb")
 	for line in file:
-		#print(line)
+		if err_count > 10:
+			print(path, "错误过多，已跳过检查")
+			break;
 		line = line.decode('latin1')
-		#print(line)
+		if crlf is None:
+			crlf = get_crlf_style(line)
+			if crlf != "windows":
+				print(path, "换行格式:", crlf)
 		line_num = line_num + 1
 		if not check_space_line(line):
 			space_line = space_line + 1
 			if(space_line > 1):
 				print(path, "第%d行:" % (line_num), "连续空行")
+				err_count = err_count + 1
 			continue
 		else:
 			space_line = 0
-		if not check_line_crlf(line):
-			print(path, "第%d行:" % (line_num), "换行不规范")
+		if not check_line_crlf(line, crlf):
+			print(path, "第%d行:" % (line_num), "换行不一致")
+			err_count = err_count + 1
 			continue
 		if not check_line_start(line):
 			print(path, "第%d行:" % (line_num), "行首有空格")
+			err_count = err_count + 1
 			continue
 		if not check_line_end(line):
 			print(path, "第%d行:" % (line_num), "行尾有空格")
+			err_count = err_count + 1
 			continue
 	file.close()
 	
+def is_source_code(path):
+	path_lower = path.lower()
+	if path_lower.endswith(".c"):
+		return True
+	if path_lower.endswith(".h"):
+		return True
+	if path_lower.endswith(".s"):
+		return True
+	return False
+
 def scan_dir_files(dir):
 	list = os.listdir(dir)
 	for item in list:
 		path = dir + "\\" + item
 		if os.path.isfile(path):
-			check_style(path)
+			if is_source_code(path):
+				check_style(path)
 		if os.path.isdir(path):
 			scan_dir_files(path)
 	return None
