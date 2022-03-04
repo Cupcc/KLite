df9efca3f2ea65a6e858d4a488dbaf3e16ba4403
jiangxiaogang <kerndev@foxmail.com>
2022-03-05 00:41:50 +0800

    添加CPU占用率统计

--- examples/nrf52832/Sources/main/daemon.c
+++ examples/nrf52832/Sources/main/daemon.c
@@ -8,10 +8,28 @@ static void daemon_thread_entry(void *arg)
 	while(1)
 	{
 		NRF_P0->OUTSET = 1<<22;
-		sleep(500);
+		sleep(250);
 		NRF_P0->OUTCLR = 1<<22;
-		sleep(500);
-		NRF_LOG_INFO("alive %d ms", kernel_time());
+		sleep(250);
+		for(int i=0; i<1000000; i++); /* eat about 18% CPU */
+	}
+}
+
+static void usage_thread_entry(void *arg)
+{
+	uint32_t tick;
+	uint32_t idle;
+	uint32_t used;
+	uint32_t free;
+	while(1)
+	{
+		tick = kernel_tick_count();
+		idle = kernel_tick_idle();
+		sleep(5000);
+		tick = kernel_tick_count() - tick;
+		idle = kernel_tick_idle() - idle;
+		heap_usage(&used, &free);
+		NRF_LOG_INFO("CPU: %d%%, HEAP: used=%d, free=%d", 100 * (tick - idle) / tick, used, free);
 	}
 }
 
@@ -20,4 +38,6 @@ void daemon_init(void)
 	thread_t thrd;
 	thrd = thread_create(daemon_thread_entry, NULL, 1024);
 	thread_set_priority(thrd, THREAD_PRIORITY_LOWEST);
+	thrd = thread_create(usage_thread_entry, NULL, 1024);
+	thread_set_priority(thrd, THREAD_PRIORITY_HIGH);
 }
