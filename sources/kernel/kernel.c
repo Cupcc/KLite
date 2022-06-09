beb54622921799df69103aa3c23321e9d0a46ff7
jiangxiaogang <kerndev@foxmail.com>
2022-06-09 20:50:52 +0800

    更新版本号5.0.1

--- sources/kernel/kernel.c
+++ sources/kernel/kernel.c
@@ -28,7 +28,7 @@
 #include "kernel.h"
 
 #define MAKE_VERSION_CODE(a,b,c)    ((a<<24)|(b<<16)|(c))
-#define KERNEL_VERSION_CODE         MAKE_VERSION_CODE(5,0,0)
+#define KERNEL_VERSION_CODE         MAKE_VERSION_CODE(5,0,1)
 
 static uint32_t m_tick_count;
 static thread_t m_idle_thread;
